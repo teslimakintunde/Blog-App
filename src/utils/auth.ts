30a2edc0b81886ce717db3f8fbc83fdd9305bfae
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-03-11 13:45:47 +0100

    correcting prisma error

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,14 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 });
